179e6af1e5a51eeb77ce7a2549a8e8f7ffc99365
이민석 <leem980425@gmail.com>
2024-11-20 12:31:03 +0900

    [Init]Base

---
+++ Assets/Scripts/Base/SingletonBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class SingletonBase<T> : MonoBehaviour where T : Component 
+{
+    private T instance;
+    public T Instance
+    {
+        get
+        {
+            instance = (T)FindObjectOfType(typeof(T));
+            if (instance == null)
+            {
+                var newObject = new GameObject(typeof(T).ToString()).AddComponent<T>();
+                DontDestroyOnLoad( newObject );
+            }
+            
+            return instance;
+        }
+    }
+
+    public void Init()
+    {
+    }
+
+}
+
---
+++ Assets/Scripts/Utils/Defines.cs
@@ -0,0 +1,30 @@
+﻿
+public enum DIR
+{
+    LEFT,
+    RIGHT,
+    TOP,
+    BOTTOM,
+    LEFTUP,
+    RIGHTUP,
+    LEFTBOTTOM,
+    RIGHTBOTTOM,
+
+    END
+}
+
+public enum PAWNTYPE
+{
+    PAWN,
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    QUEEN,
+    KING,
+}
+
+public enum COLOR
+{
+    BLACK,
+    WHITE,
+}
