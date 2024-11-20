88b0e745f11aed7af113c586ebb68a18ef493520
이민석 <leem980425@gmail.com>
2024-11-20 13:02:31 +0900

    [Base]PigGame

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public Action OnMoveEvent;
+    public Action OnAttackEvent;
+    public Action OnJumpEvent;
+
+    public void OnMove()
+    {
+
+    }
+
+    public void OnAttack()
+    {
+
+    }
+
+    public void OnJump()
+    {
+
+    }
+}
